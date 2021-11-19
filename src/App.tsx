c203f0ad475cc8ce8341d395ce3c30d5ea3f391b
flrlima <flrlima1991@gmail.com>
2021-11-19 12:23:07 -0300

    Alterando cor do thema

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import GlobalStyles from './styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 
-import dark from './styles/themes/dark';
-import light from './styles/themes/light';
+import { useTheme } from './hooks/theme';
+
 import Routes from './routes/intex';
 
 const App: React.FC = () => {
+    const {theme} = useTheme();
+
     return(
-        <ThemeProvider theme={dark}>
+        <ThemeProvider theme={theme}>
             <GlobalStyles/>
             <Routes/>
         </ThemeProvider>
